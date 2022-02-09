bef6396b414984801d3ed451e7887ace6e21a74f
Kaique Leal <leal.k.2199@gmail.com>
2022-02-09 02:01:05 -0300

    feita a base para o teste

--- test/main.js
+++ test/main.js
@@ -1,1 +1,12 @@
 import { Builder, By, Key } from "selenium-webdriver";
+import { should } from "chai";
+should();
+
+//início do script
+
+//usando describe e it do mocha
+describe("Teste técnico QA Júnior", function () {
+  it("Entra no site do youtube", async function () {
+
+  });
+});
