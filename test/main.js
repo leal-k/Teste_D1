b0a80f7457da647c4702fff58b2ddeb1866ef4f9
Kaique Leal <leal.k.2199@gmail.com>
2022-02-09 03:09:21 -0300

    entrando no youtube, digitando o canal e procurando

--- test/main.js
+++ test/main.js
@@ -7,6 +7,12 @@ should();
 //usando describe e it do mocha
 describe("Teste técnico QA Júnior", function () {
   it("Entra no site do youtube", async function () {
+    let driver = await new Builder().forBrowser("firefox").build();
+    //navegar até o site
+    await driver.get("https://www.youtube.com");
 
+    await driver.findElement(By.xpath('//*[@id="search-input"]')).click();
+    await driver.switchTo().activeElement().sendKeys("D1 Jornadas Digitais");
+    await driver.findElement(By.id('search-icon-legacy')).click();
   });
 });
